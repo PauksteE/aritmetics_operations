23450f20bdcaed70c249f1ef49181cc1c3432218
PauksteE <epaukste.vilnius@gmail.com>
2022-03-21 20:28:23 +0200

    Initial commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,22 @@
 package com.company;
 
+import java.sql.SQLOutput;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+      int val1 = 12, val2 = 5;
+
+        //addition
+        System.out.println("Val1 + val2 = : " + (val1 + val2));
+        System.out.println("val1 - val2: " + (val1 - val2));
+        System.out.println("val1 * val2: " + (val1 * val2));
+        System.out.println("val1 / val2: " + (val1 / val2));
+        System.out.println("val1 % val2: " + (val1 % val2));
+
+
+
+
     }
 }
