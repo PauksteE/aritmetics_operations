f9ae895ccbc32215d5075092eb6ea0ea5ab3765b
PauksteE <epaukste.vilnius@gmail.com>
2022-03-21 21:32:47 +0200

    Initial commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -17,6 +17,58 @@ public class Main {
 
 
 
+  //      rationalOperators();
+   //     logicalOperators();
+        percent();
+
 
     }
+
+    static void rationalOperators(){
+        int blueTeam = 20;
+        int redTeam = 25;
+
+        System.out.println(blueTeam == redTeam); //false
+
+        System.out.println(blueTeam != redTeam); // true
+
+        System.out.println(blueTeam > redTeam); // false
+
+        System.out.println(blueTeam < redTeam); // true
+
+        System.out.println(blueTeam >= redTeam); // false
+
+        System.out.println(blueTeam <= redTeam); // true
+
+    }
+
+
+    static void logicalOperators() {
+       //logical operators
+
+        System.out.println("logical operators");
+       //AND &&: returns true, if both sides are true
+        System.out.println(12 > 6 && 6<= 10); //true
+
+        // one side false, one side true
+        System.out.println(12 < 6 && 6<= 10); //false
+
+        //OR ||
+        System.out.println("OR");
+        System.out.println(12 < 6 || 6<= 10); //true
+        System.out.println(12 > 6 || 6 > 10); //true
+        System.out.println(12 < 6 || 6 > 10); //false
+
+        //NOT !
+        System.out.println("NOT");
+        System.out.println(!(12 < 6 ));
+
+    }
+
+
+    static void percent() {
+        int number = 100;
+        System.out.println("25 pers of number: " + (number + number * 0.25));
+    }
+
 }
